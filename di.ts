74a824f7d928c00ef97123993f80c6995c852928
soofka <pansoofka@gmail.com>
2023-12-01 16:30:31 +0100

    finito

--- di.ts
+++ di.ts
@@ -20,9 +20,27 @@ class WebDisplay implements IOutput {
     }
 }
 
-const app = (output: IOutput) => {
-    output.display('dupa');
+const app = (output: IOutput, text: string) => {
+    output.display(text);
 }
 
-app(new ConsoleDisplay());
-try { app(new WebDisplay()); } catch(e) {}
+app(new ConsoleDisplay(), 'dupa 1');
+app(new ConsoleDisplay(), 'dupa 2');
+app(new ConsoleDisplay(), 'dupa 3');
+try {
+    app(new WebDisplay(), 'web dupa 1');
+    app(new WebDisplay(), 'web dupa 2');
+    app(new WebDisplay(), 'web dupa 3');
+} catch(e) {}
+
+const closureApp = (output: IOutput) => (text: string) => output.display(text);
+const consoleClosureApp = closureApp(new ConsoleDisplay());
+consoleClosureApp('closure dupa 1');
+consoleClosureApp('closure dupa 2');
+consoleClosureApp('closure dupa 3');
+try {
+    const webClosureApp = closureApp(new WebDisplay());
+    webClosureApp('closure web dupa 1');
+    webClosureApp('closure web dupa 2');
+    webClosureApp('closure web dupa 3');
+} catch(e) {}
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -13,6 +13,8 @@ class User implements IDupable {
     }
 }
 
+
+
 for (let a: IDupable of []) {
     a.dupa();
 }
